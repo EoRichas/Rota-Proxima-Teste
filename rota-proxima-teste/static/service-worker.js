45c455b846afb8c5f7d6f81985e4ec11095ca78b
EoRichas <Richard.fsoarees@gmail.com>
2026-08-19 14:23:24 -0300

    Atualizar cache da dashboard centralizada

--- rota-proxima-teste/static/service-worker.js
+++ rota-proxima-teste/static/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='rota-proxima-teste-workflow-20260819-v1';
-const ASSETS=['/','/index.html','/styles.css','/workflow-patch.css','/app.js','/workflow-patch.js','/manifest.webmanifest','/icon.svg'];
+const CACHE='rota-proxima-teste-dashboard-center-20260819-v1';
+const ASSETS=['/','/index.html','/styles.css','/workflow-patch.css','/dashboard-center.css','/app.js','/workflow-patch.js','/dashboard-center.js','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{
